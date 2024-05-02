795f64842ffb4487e2893baa76bfcdc853089d10
Quelln <162735312+Quelln@users.noreply.github.com>
2024-05-02 08:19:44 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,11 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install emacs broadcom-wl iwd fish libva-intel-driver distrobox
+rpm-ostree override remove firefox firefox-langpacks qemu gamemode virtualbox-guest-additions 
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+#disabling some unneeeded services
+systemctl disable NetworkManager-wait-online ModemManager audit-rules lvm2-monitor cups.socket cups systemd-homed switcheroo-control auditd

fe57b0e1a4a7685c3d7d4e1e253f350c25ffa4e8
Quelln <162735312+Quelln@users.noreply.github.com>
2024-05-03 16:24:27 +0530

    remove broadcom-wl and iwd because they make the build not work

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install emacs broadcom-wl iwd fish libva-intel-driver distrobox
+rpm-ostree install emacs fish libva-intel-driver distrobox
 rpm-ostree override remove firefox firefox-langpacks gamemode virtualbox-guest-additions krfb krfb-libs kmousetool mariadb mariadb-server akonadi-server-mysql mariadb-server-utils fedora-chromium-config-kde plasma-browser-integration mariadb-gssapi-server mariadb-backup  mariadb-cracklib-password-check
 # this would install a package from rpmfusion
 # rpm-ostree install vlc

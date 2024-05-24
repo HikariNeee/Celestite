da4ceaa63eca62b5138dd8cd3121c3f27399d05c
Quelln <162735312+Quelln@users.noreply.github.com>
2024-05-24 15:26:40 +0530

    remove all the overrides since we use base now.

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 rpm-ostree install emacs fish libva-intel-driver distrobox
-rpm-ostree override remove firefox firefox-langpacks gamemode virtualbox-guest-additions krfb krfb-libs kmousetool mariadb mariadb-server akonadi-server-mysql mariadb-server-utils fedora-chromium-config-kde plasma-browser-integration mariadb-gssapi-server mariadb-backup  mariadb-cracklib-password-check
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 

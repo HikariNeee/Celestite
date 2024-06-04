f1144767cf544ad269d1ca11de1e8a22bdecf16a
HikariNeee <162735312+HikariNeee@users.noreply.github.com>
2024-06-04 18:10:45 +0530

    added some debug-echo statements to ublue-workaround.sh

--- rootfs/usr/etc/ublue-lightdm-workaround.sh
+++ rootfs/usr/etc/ublue-lightdm-workaround.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ ! -d /var/cache/lightdm ]; then mkdir /var/cache/lightdm; fi
-if [ ! -d /var/log/lightdm ]; then mkdir /var/log/lightdm; fi
-if [ ! -d /var/lib/lightdm ]; then mkdir /var/lib/lightdm; fi
-if [ ! -d /var/lib/lightdm-data ]; then mkdir /var/lib/lightdm-data; fi
+if [ ! -d /var/cache/lightdm ]; then echo "creating /var/cache/lightdm";mkdir /var/cache/lightdm; fi
+
+if [ ! -d /var/log/lightdm ]; then echo "creating /var/log/lightdm";mkdir /var/log/lightdm; fi
+if [ ! -d /var/lib/lightdm ]; then echo "creating /var/lib/lightdm";mkdir /var/lib/lightdm; fi
+if [ ! -d /var/lib/lightdm-data ]; then echo "creating /var/lib/lightdm-data";mkdir /var/lib/lightdm-data; fi
 
 getent group lightdm >/dev/null || groupadd -r lightdm
+echo "created lightdm group"
 getent passwd lightdm >/dev/null || useradd -g lightdm -M -d /var/lib/lightdm -s /sbin/nologin -r lightdm
-
+echo "added lightdm user"
 systemctl set-default graphical.target
+"set graphical.target as default."

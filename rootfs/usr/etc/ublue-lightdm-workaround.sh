efc610911c8e5f7150ada233e43028a07722e81b
HikariNeee <quelln@protonmail.com>
2024-06-06 14:07:51 +0530

    added a new default wallpaper from the lavanda-gtk repo

--- rootfs/usr/etc/ublue-lightdm-workaround.sh
+++ rootfs/usr/etc/ublue-lightdm-workaround.sh
@@ -12,4 +12,4 @@ getent passwd lightdm >/dev/null || useradd -g lightdm -M -d /var/lib/lightdm -s
 echo "added lightdm user"
 systemctl set-default graphical.target
 "set graphical.target as default."
-echo "background=/usr/share/backgrounds/default_back.jpg" | tee -a /etc/lightdm/slick-greeter.conf
+echo "background=/usr/share/backgrounds/default_back.png" | tee -a /etc/lightdm/slick-greeter.conf

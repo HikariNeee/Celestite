813b6e704303c5f44b93d87dd7ce7eef958a554b
HikariNeee <quelln@protonmail.com>
2024-06-14 23:34:39 +0530

    Switched to Otis-forest theme and dark mode by default.

--- rootfs/usr/share/firstboot/set-theme.sh
+++ rootfs/usr/share/firstboot/set-theme.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir "${HOME}/.config/gtk-4.0"
-ln -sf /usr/share/themes/Lavanda-Sea-Light/gtk-4.0/gtk.css "${HOME}/.config/gtk-4.0/gtk.css"
-ln -sf /usr/share/themes/Lavanda-Sea-light/gtk-4.0/assets  "${HOME}/.config/gtk-4.0/assets"
-ln -sf /usr/share/themes/Lavanda-Sea-Light/gtk-4.0/gtk-dark.css  "${HOME}/.config/gtk-4.0/gtk-dark.css"
+ln -sf /usr/share/themes/Otis-forest-standard-buttons/gtk-4.0/gtk.css "${HOME}/.config/gtk-4.0/gtk.css"
+ln -sf /usr/share/themes/Otis-forest-standard-buttons/assets  "${HOME}/.config/gtk-4.0/assets"
+ln -sf /usr/share/themes/Otis-forest-standard-buttons/gtk-4.0/gtk-dark.css  "${HOME}/.config/gtk-4.0/gtk-dark.css"
 
 
 echo "setting lavanda as flatpak style"
-/usr/share/firstboot/stylepak  install-user Lavanda-Sea-Light
-/usr/share/firstboot/stylepak install-system Lavanda-Sea-Light
+/usr/share/firstboot/stylepak  install-user Otis-forest-standard-buttons
+/usr/share/firstboot/stylepak install-system Otis-forest-standard-buttons
 
 

15fd23b0f93941d01fcf55f42fed8c09528f8924
Hikari <162735312+HikariNeee@users.noreply.github.com>
2024-11-21 13:07:37 +0530

    Switch to Fedora 41 (#11)
    * Add files via upload
    
    * Add files via upload
    
    * fix build issues
    
    * Bump sigstore/cosign-installer from 3.5.0 to 3.6.0 (#9)
    
    Bumps [sigstore/cosign-installer](https://github.com/sigstore/cosign-installer) from 3.5.0 to 3.6.0.
    - [Release notes](https://github.com/sigstore/cosign-installer/releases)
    - [Commits](https://github.com/sigstore/cosign-installer/compare/v3.5.0...v3.6.0)
    
    ---
    updated-dependencies:
    - dependency-name: sigstore/cosign-installer
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump sigstore/cosign-installer from 3.6.0 to 3.7.0 (#10)
    
    Bumps [sigstore/cosign-installer](https://github.com/sigstore/cosign-installer) from 3.6.0 to 3.7.0.
    - [Release notes](https://github.com/sigstore/cosign-installer/releases)
    - [Commits](https://github.com/sigstore/cosign-installer/compare/v3.6.0...v3.7.0)
    
    ---
    updated-dependencies:
    - dependency-name: sigstore/cosign-installer
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * remove intel-media-driver
    
    * feat: Rebase to F41, change theme to rose-pine, set default font to IBM Plex
    
    * feat: added new logo for plymouth, make the workflows a bit better and fix the nvidia build
    
    * chore: clean up files a bit, remove redundant settings use some of cachyOS's tweaks
    
    * remove nvidia-sleep, nvidia-resume
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: OrioleNix <163466443+OrioleNix@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/02-misc.sh
+++ scripts/02-misc.sh
@@ -2,12 +2,6 @@
 
 set -ouex pipefail
 
-curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
-tar -xzf /tmp/starship.tar.gz -C /tmp
-install -c -m 0755 /tmp/starship /usr/bin
-
-echo 'eval "$(starship init bash)"' >> /etc/bashrc
-
 rpm-ostree install btop lshw xdotool wmctrl
 
 echo "Hidden=true" >> /usr/share/applications/fish.desktop
